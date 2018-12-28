ae5124c88e9dfcae955a07a5a26d94b1fcdc48ce
jamesta696 <jaysmith696@gmail.com>
2018-12-27 20:38:41 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without an error", () => {
+    const wrapper = shallow(<App />);
+    const appComponent = wrapper.find("[data-test='app-component']");
+    expect(appComponent.length).toBe(1);
 });
+
+test("renders the increment button", () => {
+    const wrapper = shallow(<App />);
+    const button = wrapper.find("[data-test='increment-button-component']");
+    expect(button.length).toBe(1);
+});
+
+test("renders the increment button", () => {});
+
+test("renders the counter display", () => {});
+
+test("coutner starts at 0", () => {});
+
+test("when button clicked, the counter is incremented in display", () => {});
